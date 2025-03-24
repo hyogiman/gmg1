8b0a7825bb373b9002e283c41a09e4943b8edb11
hyogiman <77236186+hyogiman@users.noreply.github.com>
2025-03-25 01:37:49 +0900

    Add files via upload

---
+++ assets/js/firebase-init.js
@@ -0,0 +1,12 @@
+// Firebase SDK 초기화
+const firebaseConfig = {
+  apiKey: "AIzaSyCCxRR6hXvk9Z17eK8H_Vov_TbGX8LzvDs",
+  authDomain: "factory-sim01v.firebaseapp.com",
+  projectId: "factory-sim01v",
+  storageBucket: "factory-sim01v.firebasestorage.app",
+  messagingSenderId: "818674298880",
+  appId: "1:818674298880:web:95307171220da8bd8d28e5"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
