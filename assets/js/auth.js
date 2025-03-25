3031a03d1b347bff45b869831bf3df28cf7c906a
hyogiman <77236186+hyogiman@users.noreply.github.com>
2025-03-25 13:41:21 +0900

    Update auth.js

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -2,14 +2,26 @@ async function login() {
   const teamId = document.getElementById("teamId").value.trim();
   const pw = document.getElementById("teamPw").value.trim();
 
-  if (!teamId || !pw) return alert("ID와 비밀번호를 입력하세요.");
+  if (!teamId || !pw) {
+    alert("ID와 비밀번호를 입력하세요.");
+    return;
+  }
 
-  const doc = await db.collection("teams").doc(teamId).get();
-  if (!doc.exists) return alert("존재하지 않는 팀입니다.");
+  const ref = db.collection("teams").doc(teamId);
+  const doc = await ref.get();
+
+  if (!doc.exists) {
+    alert("존재하지 않는 팀입니다.");
+    return;
+  }
 
   const data = doc.data();
-  if (data.password !== pw) return alert("비밀번호가 틀렸습니다.");
+  if (data.password !== pw) {
+    alert("비밀번호가 틀렸습니다.");
+    return;
+  }
 
   localStorage.setItem("currentTeam", teamId);
+  alert("✅ 로그인 성공!");
   location.href = "home.html";
 }
